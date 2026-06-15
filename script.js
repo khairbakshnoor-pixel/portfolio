55fa3ee60d605f1ee7fea51437ff8d5072dac822
KHAIR BAKHSH <khairbakshnoor@gmail.com>
2026-06-15 15:05:20 +0500

    addding

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple interactive message
+console.log("Portfolio Loaded Successfully 🚀");
+
+// Example: small animation effect
+document.addEventListener("DOMContentLoaded", function () {
+  alert("Welcome to Khair Bakhsh Noor Dad Portfolio 🚀");
+});
